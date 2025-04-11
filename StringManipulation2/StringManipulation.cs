df3d9800ca74a6e87b08ceef020a2bc36fcb4246
chris kalkas <chris.kalkas@icloud.com>
2025-04-11 11:42:25 -0500

    Last Update

--- StringManipulation2/StringManipulation.cs
+++ StringManipulation2/StringManipulation.cs
@@ -82,17 +82,18 @@ namespace FindAWord
             //Date in the Year/Month/Day format.
             String Pattern = "^[\\d]{4}[\\\\/-][\\d]{2}[\\\\/-][\\d]{2}";
             //Date in the Day/Month/Year or Month/Day/Year format.
-            String Pattern2 = "[\\d]{2}[\\\\/-][\\d]{2}F[\\\\/-][\\d]{4}";
+            String Pattern2 = "[\\d]{2}[\\\\/-][\\d]{2}[\\\\/-][\\d]{4}";
             Regex rg = new(Pattern);
             Regex rg2 = new(Pattern2);
             int nextstart = 0;
             if (rg.IsMatch(InputString))
             {
+                //creates an array to store date substrings
                 String[] dateParts = new string[3];
                 dateParts[0] = InputString.Substring(0, 4);
                 dateParts[1] = InputString.Substring(5, 2);
                 dateParts[2] = InputString.Substring(8, 2);
-                month =Convert.ToInt32(dateParts[1]);
+                month = Convert.ToInt32(dateParts[1]);
 
 
                 switch (month) {
@@ -101,41 +102,51 @@ namespace FindAWord
                         {
                             match = false;
                         }
-                        else { 
-                        
-                        match = true;
+                        else {
+
+                            match = true;
                         }
 
-                    break;
+                        break;
                     case 02:
+                        //Checks For Leapyear
                         if (Convert.ToInt32(dateParts[2]) > 29)
                         {
                             match = false;
                         }
                         else
                         {
-                            for (int i = Leapyear; i < Convert.ToInt16(dateParts[0]); i+=4)
+                            match = true;
+                            for (int i = Leapyear; i < Convert.ToInt16(dateParts[0]); i += 4)
                             {
                                 Leapyear = Leapyear + 4;
                             }
-
-                                if (Convert.ToInt32(dateParts[0]) == Leapyear && Convert.ToInt32(dateParts[2])==29)
+                            if (Convert.ToInt16(dateParts[0]) < Leapyear) {
+                                for (int i = Leapyear; i > Convert.ToInt16(dateParts[0]); i -= 4)
                                 {
-
-                                    match = true;
+                                    Leapyear = Leapyear - 4;
                                 }
+                            }
+                            if (Convert.ToInt32(dateParts[0]) == Leapyear && Convert.ToInt32(dateParts[2]) <= 29 || Convert.ToInt32(dateParts[2])<=28)
+                            {
 
-                                else
-                                {
-                                    match = false;
-                                }
+                                match = true;
+                            }
+                            else
+                            {
+                                match = false;
+                            }
+
                             
+
                             
+
                         }
                         
 
+
                         break;
-                    
+
                     case 03:
                         if (Convert.ToInt32(dateParts[2]) > 31)
                         {
@@ -148,8 +159,8 @@ namespace FindAWord
                         }
 
                         break;
-                   
-                    
+
+
                     case 04:
                         if (Convert.ToInt32(dateParts[2]) > 30)
                         {
@@ -268,21 +279,217 @@ namespace FindAWord
                         break;
                 }
 
-                if (Convert.ToInt32(dateParts[2]) > 31)
-                {
-                    match = false;
-                }
 
 
 
             }
+            //verifies if dd/mm/yyyy is a correct date
             if (rg2.IsMatch(InputString))
             {
+                //creates an array to store date substrings
+                String[] dateParts = new string[3];
+                dateParts[2] = InputString.Substring(6, 4);
+                dateParts[1] = InputString.Substring(3, 2);
+                dateParts[0] = InputString.Substring(0, 2);
+                month = Convert.ToInt32(dateParts[1]);
 
-                match = true;
+                //Checks each month in dateParts
+                switch (month)
+                {
+                    case 01:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+                    case 02:
+                        if (Convert.ToInt32(dateParts[1]) > 29)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+                            //Checks for Leapyear
+                            for (int i = Leapyear; i < Convert.ToInt16(dateParts[2]); i += 4)
+                            {
+                                Leapyear = Leapyear + 4;
+                            }
+                            if (Convert.ToInt16(dateParts[2]) < Leapyear)
+                            {
+                                for (int i = Leapyear; i > Convert.ToInt16(dateParts[2]); i -= 4)
+                                {
+                                    Leapyear = Leapyear - 4;
+                                }
+                            }
+                            if (Convert.ToInt32(dateParts[2]) == Leapyear && Convert.ToInt32(dateParts[0]) <= 29 || Convert.ToInt32(dateParts[0]) <= 28)
+                            {
+
+                                match = true;
+                            }
+
+                            else
+                            {
+                                match = false;
+                            }
+
+
+                        }
+
+
+                        break;
+
+                    case 03:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+
+                    case 04:
+                        if (Convert.ToInt32(dateParts[0]) > 30)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 05:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 06:
+                        if (Convert.ToInt32(dateParts[0]) > 30)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 07:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 08:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 09:
+                        if (Convert.ToInt32(dateParts[0]) > 30)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 10:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 11:
+                        if (Convert.ToInt32(dateParts[0]) > 30)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+
+                    case 12:
+                        if (Convert.ToInt32(dateParts[0]) > 31)
+                        {
+                            match = false;
+                        }
+                        else
+                        {
+
+                            match = true;
+                        }
+
+                        break;
+                }
+
+                
+
+
+
             }
+
+
+
+           
 
             return match;
         }
+
+           
+        
     } 
 }
